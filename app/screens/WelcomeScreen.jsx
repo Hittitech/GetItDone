e136a79cad0113c59aa2029a0869efdcab3f2151
Hittitech <mdadnansami9381@gmail.com>
2024-07-23 18:04:16 +0530

    Created Welcome Screen

---
+++ app/screens/WelcomeScreen.jsx
@@ -0,0 +1,52 @@
+import { Image, ImageBackground, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import AppButton from "../components/AppButton";
+import colors from "../config/colors";
+
+export default function WelcomeScreen() {
+  return (
+    <ImageBackground
+      blurRadius={10}
+      style={styles.background}
+      source={require("../../assets/images/background.jpeg")}
+    >
+      <View style={styles.logoContainer}>
+        <Image
+          style={styles.logo}
+          source={require("@/assets/images/react-logo.png")}
+        />
+        <Text style={styles.tagLine}>Sell What You Don't Need</Text>
+      </View>
+      <View style={styles.buttonsContainer}>
+        <AppButton title={"Login"} />
+        <AppButton title={"Register"} color={colors.secondary} />
+      </View>
+    </ImageBackground>
+  );
+}
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  buttonsContainer: {
+    padding: 20,
+    width: "100%",
+  },
+  logoContainer: {
+    position: "absolute",
+    top: 70,
+    alignItems: "center",
+  },
+  logo: {
+    width: 100,
+    height: 100,
+  },
+  tagLine: {
+    fontSize: 25,
+    fontWeight: "600",
+    paddingVertical: 10,
+  },
+});
