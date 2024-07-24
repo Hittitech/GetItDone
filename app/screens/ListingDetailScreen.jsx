33bf714c89dc2a5211409052a9bfc6035f63fe56
Hittitech <mdadnansami9381@gmail.com>
2024-07-24 22:30:31 +0530

    Created Listing detail screen

---
+++ app/screens/ListingDetailScreen.jsx
@@ -0,0 +1,52 @@
+import { Image, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import AppText from "../components/AppText";
+import colors from "../config/colors";
+import ListItem from "../components/ListItem";
+
+const ListingDetailScreen = () => {
+  return (
+    <View>
+      <Image
+        style={styles.image}
+        source={require("../../assets/images/jacket.jpg")}
+      />
+      <View style={styles.textContainer}>
+        <AppText style={styles.title}>Red Jacket For Sale</AppText>
+        <AppText style={styles.price}>$100</AppText>
+        <View style={styles.userContainer}>
+          <ListItem
+            image={require("@/assets/images/react-logo.png")}
+            title={"Adnan Sami"}
+            subtitle={"5 listings"}
+          />
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default ListingDetailScreen;
+
+const styles = StyleSheet.create({
+  image: {
+    width: "100%",
+    height: 300,
+  },
+  price: {
+    color: colors.secondary,
+    fontWeight: "bold",
+    fontSize: 20,
+    marginVertical: 10,
+  },
+  textContainer: {
+    padding: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "500",
+  },
+  userContainer: {
+    marginVertical: 30,
+  },
+});
